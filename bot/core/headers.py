f4bca3e98c64e8dae224b6ab517f211ff048e83d
argoarsiks <smetankauwu3008@gmail.com>
2025-05-25 14:43:55 +0300

    Add headers for requests

---
+++ bot/core/headers.py
@@ -0,0 +1,15 @@
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ru,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/json",
+    "Host": "api.qlyuker.io",
+    "Klyuk": "0110101101101100011011110110111101101011",
+    "Locale": "ru",
+    "Onboarding": "2",
+    "Origin": "https://qlyuker.io",
+    "Pragma": "no-cache",
+    "Referer": "https://qlyuker.io/",
+    "Tgplatform": "ios"
+}
