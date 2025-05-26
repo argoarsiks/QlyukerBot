7e0cfe3fc83feb42aacffbb1e6c09dbd5b399a72
argoarsiks <smetankauwu3008@gmail.com>
2025-05-26 03:09:50 +0300

    Add main.py withou argparse

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import json
+import asyncio
+import argparse
+from bot.core import mainLoop
+from bot.utils import regSession
+
+
+async def defConfig(sessionName):
+    with open(f"{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', sessionName)}.json", "w", encoding="utf-8") as f:
+        cfg = {
+            "autoTaps": True,
+            "autoUpgrade": True,
+            "tapsPerCycle": "5;15",
+            "sleep": "100;1000",
+            "proxy": None
+        }
+        json.dump(cfg, f, ensure_ascii=False)
+
+
+async def main():
+    print("""   
+██████╗░██╗░░░██╗  ░█████╗░██████╗░░██████╗░░█████╗░░█████╗░██████╗░░██████╗
+██╔══██╗╚██╗░██╔╝  ██╔══██╗██╔══██╗██╔════╝░██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██████╦╝░╚████╔╝░  ███████║██████╔╝██║░░██╗░██║░░██║███████║██████╔╝╚█████╗░
+██╔══██╗░░╚██╔╝░░  ██╔══██║██╔══██╗██║░░╚██╗██║░░██║██╔══██║██╔══██╗░╚═══██╗
+██████╦╝░░░██║░░░  ██║░░██║██║░░██║╚██████╔╝╚█████╔╝██║░░██║██║░░██║██████╔╝
+╚═════╝░░░░╚═╝░░░  ╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+          """)
+    print(f"Select a mode:\n\n"
+          f"1: Create a new session\n\n"
+          f"2: Starting a bot\n")
+    inp = int(input("Your choice: "))
+    if inp == 1:
+        inpName = str(input("Enter a title for the session: "))
+        resultReg = await regSession(sessionName=inpName)
+        if resultReg == True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"Select a config:\n\n"
+                  f"1: Default config (legit)\n\n"
+                  f"2: Custom config\n")
+            inpConfigChoice = int(input("Your choice: "))
+            if inpConfigChoice == 1:
+                await defConfig(inpName)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"Config successfully saved (It can always be changed in the 'config' directory). To run the bot, restart the script")
+            elif inpConfigChoice == 2:
+                await defConfig(inpName)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                config_path = f"{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', inpName)}.json"
+                print(f"You can change the config along the way: {config_path}\n"
+                      f"To run the bot, restart the script")
+    elif inp == 2:
+        session_names = []
+        for filename in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "sessions")):
+            if filename.endswith(".session"):
+                session_names.append(os.path.splitext(filename)[0])
+        tasks = [mainLoop(sessionName) for sessionName in session_names]
+        await asyncio.gather(*tasks)
+                
+
+if __name__ == "__main__":
+    asyncio.run(main())
