a1df685a4fbfbea1c4b527c733af2a42d1e50582
argoarsiks <smetankauwu3008@gmail.com>
2025-12-10 22:09:39 +0300

    feat: add get_all_sessions func

--- src/utils/tg_session.py
+++ src/utils/tg_session.py
@@ -2,6 +2,7 @@ from pyrogram import Client
 from pyrogram.raw.functions.messages import RequestWebView
 from urllib.parse import urlparse, parse_qs, unquote
 from pathlib import Path
+import os
 
 SESSION_DIR_PATH = Path(__file__).parent.parent.parent / "sessions"
 
@@ -37,3 +38,13 @@ async def get_web_app(sesion_name: str, api_id: int, api_hash: str) -> str:
 async def create_session(session_name: str, api_id: int, api_hash: str) -> None:
     async with Client(session_name, api_id, api_hash, workdir=SESSION_DIR_PATH) as client:
         await client.get_me()
+
+
+def get_all_sessions() -> list[str]:
+    sessions = []
+
+    for file in os.listdir(SESSION_DIR_PATH):
+        if file.endswith("session"):
+            sessions.append(file[:-8])
+
+    return sessions
